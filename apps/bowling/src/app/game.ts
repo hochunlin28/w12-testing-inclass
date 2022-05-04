7d180128271562111454c85608544125833f144f
HoChunLin <hochunlin28@gmail.com>
2022-05-04 21:01:37 +0800

    The fourth green light

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -4,19 +4,18 @@ export class Game {
     roll(pins: number) {
       this.rolls[this.currentRoll++] = pins;
     }
-    
+  
     get score() {
       let score = 0;
       let frameIndex = 0;
       for (let frame = 0; frame < 10; frame++) {
-        if (this.rolls[frameIndex] === 10) {
-            score += 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
-            frameIndex++;
-            continue;
+        if (this.isStrike(frameIndex)) {
+          score += 10 + this.strikeBouns(frameIndex);
+          frameIndex++;
+          continue;
         }
-
         if (this.isSpare(frameIndex)) {
-          score += 10 + this.rolls[frameIndex + 2];
+          score += 10 + this.spareBouns(frameIndex);
         } else {
           score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
         }
@@ -25,7 +24,19 @@ export class Game {
       return score;
     }
   
+    private isStrike(frameIndex: number) {
+      return this.rolls[frameIndex] === 10;
+    }
+  
     private isSpare(frameIndex: number) {
       return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+    }
+  
+    private strikeBouns(frameIndex: number) {
+      return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+    }
+  
+    private spareBouns(frameIndex: number) {
+      return this.rolls[frameIndex + 2];
     }
   }
